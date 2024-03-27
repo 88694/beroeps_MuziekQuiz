945218dc6b755801402a85b5e38da86f5e881e6f
88694 <88694@glr.nl>
2024-03-27 12:02:12 +0100

    beta v.1.1

--- muziek_quiz/src/javascript/script.js
+++ muziek_quiz/src/javascript/script.js
@@ -1,122 +1,215 @@
-const questionContainer = document.getElementById("question-container");
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("next-btn");
-const restartButton = document.getElementById("restart-btn");
-const resultDiv = document.getElementById("result");
+const progressBar = document.querySelector(".progress-bar"),
+  progressText = document.querySelector(".progress-text");
 
-let shuffledQuestions, currentQuestionIndex, score;
+const progress = (value) => {
+  const percentage = (value / time) * 100;
+  progressBar.style.width = `${percentage}%`;
+  progressText.innerHTML = `${value}`;
+};
 
-const questions = [
-  {
-    question: "What is 2 + 2?",
-    answers: [
-      { text: "4", correct: true },
-      { text: "22", correct: false },
-      { text: "222", correct: false },
-      { text: "2222", correct: false },
-    ],
-  },
-  {
-    question: "What does HTML stand for?",
-    answers: [
-      { text: "HyperText Markup Language", correct: true },
-      { text: "HighText Machine Language", correct: false },
-      { text: "HyperText Machine Language", correct: false },
-      { text: "HighText Markup Language", correct: false },
-    ],
-  },
-  {
-    question: "Which property is used to change the background color?",
-    answers: [
-      { text: "color", correct: false },
-      { text: "bgColor", correct: false },
-      { text: "background-color", correct: true },
-      { text: "background", correct: false },
-    ],
-  },
-  {
-    question: "Inside which HTML element do we put the JavaScript?",
-    answers: [
-      { text: "<js>", correct: false },
-      { text: "<javascript>", correct: false },
-      { text: "<script>", correct: true },
-      { text: "<scripting>", correct: false },
-    ],
-  },
-];
+const startBtn = document.querySelector(".start"),
+  numQuestions = document.querySelector("#num-questions"),
+  category = document.querySelector("#category"),
+  difficulty = document.querySelector("#difficulty"),
+  timePerQuestion = document.querySelector("#time"),
+  quiz = document.querySelector(".quiz"),
+  startScreen = document.querySelector(".start-screen");
 
-startQuiz();
+let questions = [],
+  time = 30,
+  score = 0,
+  currentQuestion,
+  timer;
 
-function startQuiz() {
-  score = 0;
-  questionContainer.style.display = "flex";
-  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
-  currentQuestionIndex = 0;
-  nextButton.classList.remove("hide");
-  restartButton.classList.add("hide");
-  resultDiv.classList.add("hide");
-  setNextQuestion();
-}
+const startQuiz = () => {
+  const num = numQuestions.value,
+    cat = category.value,
+    diff = difficulty.value;
+  loadingAnimation();
+  const url = `https://opentdb.com/api.php?amount=${num}&category=${cat}&difficulty=${diff}&type=multiple`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      questions = data.results;
+      setTimeout(() => {
+        startScreen.classList.add("hide");
+        quiz.classList.remove("hide");
+        currentQuestion = 1;
+        showQuestion(questions[0]);
+      }, 1000);
+    });
+};
 
-function setNextQuestion() {
-  resetState();
-  showQuestion(shuffledQuestions[currentQuestionIndex]);
-}
+startBtn.addEventListener("click", startQuiz);
 
-function showQuestion(question) {
-  questionElement.innerText = question.question;
-  question.answers.forEach((answer, index) => {
-    const inputGroup = document.createElement("div");
-    inputGroup.classList.add("input-group");
+const showQuestion = (question) => {
+  const questionText = document.querySelector(".question"),
+    answersWrapper = document.querySelector(".answer-wrapper");
+  questionNumber = document.querySelector(".number");
 
-    const radio = document.createElement("input");
-    radio.type = "radio";
-    radio.id = "answer" + index;
-    radio.name = "answer";
-    radio.value = index;
+  questionText.innerHTML = question.question;
 
-    const label = document.createElement("label");
-    label.htmlFor = "answer" + index;
-    label.innerText = answer.text;
-
-    inputGroup.appendChild(radio);
-    inputGroup.appendChild(label);
-    answerButtons.appendChild(inputGroup);
+  const answers = [
+    ...question.incorrect_answers,
+    question.correct_answer.toString(),
+  ];
+  answersWrapper.innerHTML = "";
+  answers.sort(() => Math.random() - 0.5);
+  answers.forEach((answer) => {
+    answersWrapper.innerHTML += `
+                  <div class="answer ">
+            <span class="text">${answer}</span>
+            <span class="checkbox">
+              <i class="fas fa-check"></i>
+            </span>
+          </div>
+        `;
   });
-}
 
-function resetState() {
-  while (answerButtons.firstChild) {
-    answerButtons.removeChild(answerButtons.firstChild);
-  }
-}
+  questionNumber.innerHTML = ` Question <span class="current">${
+    questions.indexOf(question) + 1
+  }</span>
+            <span class="total">/${questions.length}</span>`;
+  //add event listener to each answer
+  const answersDiv = document.querySelectorAll(".answer");
+  answersDiv.forEach((answer) => {
+    answer.addEventListener("click", () => {
+      if (!answer.classList.contains("checked")) {
+        answersDiv.forEach((answer) => {
+          answer.classList.remove("selected");
+        });
+        answer.classList.add("selected");
+        submitBtn.disabled = false;
+      }
+    });
+  });
 
-nextButton.addEventListener("click", () => {
-  const answerIndex = Array.from(
-    answerButtons.querySelectorAll("input")
-  ).findIndex((radio) => radio.checked);
-  if (answerIndex !== -1) {
-    if (shuffledQuestions[currentQuestionIndex].answers[answerIndex].correct) {
-      score++;
+  time = timePerQuestion.value;
+  startTimer(time);
+};
+
+const startTimer = (time) => {
+  timer = setInterval(() => {
+    if (time === 3) {
+      playAdudio("countdown.mp3");
     }
-    currentQuestionIndex++;
-    if (shuffledQuestions.length > currentQuestionIndex) {
-      setNextQuestion();
+    if (time >= 0) {
+      progress(time);
+      time--;
     } else {
-      endQuiz();
+      checkAnswer();
     }
-  } else {
-    alert("Please select an answer.");
-  }
+  }, 1000);
+};
+
+const loadingAnimation = () => {
+  startBtn.innerHTML = "Loading";
+  const loadingInterval = setInterval(() => {
+    if (startBtn.innerHTML.length === 10) {
+      startBtn.innerHTML = "Loading";
+    } else {
+      startBtn.innerHTML += ".";
+    }
+  }, 500);
+};
+function defineProperty() {
+  var osccred = document.createElement("div");
+  osccred.style.position = "absolute";
+  osccred.style.bottom = "0";
+  osccred.style.right = "0";
+  osccred.style.fontSize = "10px";
+  osccred.style.color = "#ccc";
+  osccred.style.fontFamily = "sans-serif";
+  osccred.style.padding = "5px";
+  osccred.style.background = "#fff";
+  osccred.style.borderTopLeftRadius = "5px";
+  osccred.style.borderBottomRightRadius = "5px";
+  osccred.style.boxShadow = "0 0 5px #ccc";
+  document.body.appendChild(osccred);
+}
+
+defineProperty();
+
+const submitBtn = document.querySelector(".submit"),
+  nextBtn = document.querySelector(".next");
+submitBtn.addEventListener("click", () => {
+  checkAnswer();
 });
 
-restartButton.addEventListener("click", startQuiz);
+nextBtn.addEventListener("click", () => {
+  nextQuestion();
+  submitBtn.style.display = "block";
+  nextBtn.style.display = "none";
+});
 
-function endQuiz() {
-  questionContainer.style.display = "none";
-  nextButton.classList.add("hide");
-  restartButton.classList.remove("hide");
-  resultDiv.classList.remove("hide");
-  resultDiv.innerText = `Your final score: ${score} / ${shuffledQuestions.length}`;
-}
+const checkAnswer = () => {
+  clearInterval(timer);
+  const selectedAnswer = document.querySelector(".answer.selected");
+  if (selectedAnswer) {
+    const answer = selectedAnswer.querySelector(".text").innerHTML;
+    console.log(currentQuestion);
+    if (answer === questions[currentQuestion - 1].correct_answer) {
+      score++;
+      selectedAnswer.classList.add("correct");
+    } else {
+      selectedAnswer.classList.add("wrong");
+      const correctAnswer = document
+        .querySelectorAll(".answer")
+        .forEach((answer) => {
+          if (
+            answer.querySelector(".text").innerHTML ===
+            questions[currentQuestion - 1].correct_answer
+          ) {
+            answer.classList.add("correct");
+          }
+        });
+    }
+  } else {
+    const correctAnswer = document
+      .querySelectorAll(".answer")
+      .forEach((answer) => {
+        if (
+          answer.querySelector(".text").innerHTML ===
+          questions[currentQuestion - 1].correct_answer
+        ) {
+          answer.classList.add("correct");
+        }
+      });
+  }
+  const answersDiv = document.querySelectorAll(".answer");
+  answersDiv.forEach((answer) => {
+    answer.classList.add("checked");
+  });
+
+  submitBtn.style.display = "none";
+  nextBtn.style.display = "block";
+};
+
+const nextQuestion = () => {
+  if (currentQuestion < questions.length) {
+    currentQuestion++;
+    showQuestion(questions[currentQuestion - 1]);
+  } else {
+    showScore();
+  }
+};
+
+const endScreen = document.querySelector(".end-screen"),
+  finalScore = document.querySelector(".final-score"),
+  totalScore = document.querySelector(".total-score");
+const showScore = () => {
+  endScreen.classList.remove("hide");
+  quiz.classList.add("hide");
+  finalScore.innerHTML = score;
+  totalScore.innerHTML = `/ ${questions.length}`;
+};
+
+const restartBtn = document.querySelector(".restart");
+restartBtn.addEventListener("click", () => {
+  window.location.reload();
+});
+
+const playAdudio = (src) => {
+  const audio = new Audio(src);
+  audio.play();
+};
